89cecf01b9e66bff4be7ab17b5273e387200c15e
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2023-10-25 20:51:57 +0530

    Update collectingTweetsMoreThan20.py

--- collectingTweetsMoreThan20.py
+++ collectingTweetsMoreThan20.py
@@ -3,17 +3,17 @@ from collectingInformationFromTwitter import OAuthVerifier
 
 def getUserFollowers(api):
         print('\nFollowers:')
-        for follower in tweepy.cursor(api.followers()).items():
+        for follower in tweepy.Cursor(api.followers()).items():
                 print(follower.screen_name)
 
 def getUserFriends(api):
         print('\n Friends:')
-        for friend in tweepy.cursor(api.friends()).items():
+        for friend in tweepy.Cursor(api.friends()).items():
                 print(friend.screen_name)
 
 def getUserTweets(api):
         print('\n Tweets : ')
-        for tweet in tweepy.cursor(api.user_timeline()).items():
+        for tweet in tweepy.Cursor(api.user_timeline()).items():
                 print(tweet.text)
 
 def main():
